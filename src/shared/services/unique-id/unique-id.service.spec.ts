6c18de7040e5d3d17195bf65ec88d81b4726e973
Arian Botine <arian.botine@shift.com.br>
2022-10-23 17:53:12 -0300

    Aprendendo a criar validação de throw

--- src/shared/services/unique-id/unique-id.service.spec.ts
+++ src/shared/services/unique-id/unique-id.service.spec.ts
@@ -5,12 +5,14 @@ describe(UniqueIdService.name, () => {
     service = new UniqueIdService();
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+   should generate id when called with prefix`, () => {
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
   });
 
-  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate id duplicate when called multiple times`, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+   should not generate id duplicate when called multiple times`, () => {
     const numberOfGeneratedIds = 50;
     const ids = new Set();
     for (let i = 0; i < numberOfGeneratedIds; i++) {
@@ -19,9 +21,30 @@ describe(UniqueIdService.name, () => {
     expect(ids.size).toBe(50);
   });
 
-  it(`#${UniqueIdService.prototype.getNumberOfGeneratedIds.name} should return the number of generatedIds when called`, () => {
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedIds.name}
+   should return the number of generatedIds when called`, () => {
     service.generateUniqueIdWithPrefix('app');
     service.generateUniqueIdWithPrefix('app');
     expect(service.getNumberOfGeneratedIds()).toBe(2);
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+   should throw error when called with parameter empty`, () => {
+    const emptyValues = [null, '', undefined];
+    emptyValues.forEach((emptyValue) => {
+      expect(() => {
+        service.generateUniqueIdWithPrefix(emptyValue);
+      }).toThrow();
+    });
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name}
+   should throw error when called with parameter that contain number`, () => {
+    const valuesContainingNumbers = ['54asd', '654'];
+    valuesContainingNumbers.forEach((valuesContainingNumber) => {
+      expect(() => {
+        service.generateUniqueIdWithPrefix(valuesContainingNumber);
+      }).toThrow();
+    });
   });
 });
